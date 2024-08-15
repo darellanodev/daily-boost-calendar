6b8c331b8788b27f45f6a775fd979bff4d344ef1
darellanodev <darellano.aliaga@gmail.com>
2024-08-15 16:25:35 +0200

    fill with left zero the day and the month #1

--- tests/unit/ActiveDays.test.ts
+++ tests/unit/ActiveDays.test.ts
@@ -1,17 +1,17 @@
 import { ActiveDays } from "../../src/ActiveDays"
 import { Day } from "../../src/Day"
 
-const activeDaysList = ["5/7/2024", "8/7/2024"]
+const activeDaysList = ["05/07/2024", "08/07/2024"]
 const activeDays = new ActiveDays(activeDaysList)
 
 test("isActiveDay, if is in the ActiveDays, returns true", () => {
-  const day = new Day("8/7/2024")
+  const day = new Day("08/07/2024")
   const result = activeDays.isActiveDay(day)
   expect(result).toBeTruthy()
 })
 
 test("isActiveDay, if is not in the ActiveDays, returns false", () => {
-  const day = new Day("2/7/2024")
+  const day = new Day("02/07/2024")
   const result = activeDays.isActiveDay(day)
   expect(result).toBeFalsy()
 })
