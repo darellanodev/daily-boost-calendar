5d3971b0e412fc75f330bbe08977a5388f9fac06
darellanodev <darellano.aliaga@gmail.com>
2024-09-07 19:50:55 +0200

    #7 highlight the active days

--- src/Calendar/index.tsx
+++ src/Calendar/index.tsx
@@ -9,6 +9,17 @@ interface CalendarProps {
 }
 
 export function Calendar({ title }: CalendarProps) {
+  const activeDaysList = [
+    '06/08/2024',
+    '07/08/2024',
+    '09/08/2024',
+    '11/08/2024',
+    '13/08/2024',
+    '14/08/2024',
+    '16/08/2024',
+    '18/08/2024',
+  ]
+
   const totalWeeks = 4
   const datesProvider = new DatesProvider(totalWeeks)
   datesProvider.calculate()
@@ -18,7 +29,8 @@ export function Calendar({ title }: CalendarProps) {
   const daysItems: DayItem[] = []
   let i = 1
   for (const date of dates) {
-    daysItems.push(new DayItem(i, date, false))
+    let completed = activeDaysList.includes(date)
+    daysItems.push(new DayItem(i, date, completed))
     i++
   }
 
