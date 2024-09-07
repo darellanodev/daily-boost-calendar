d620c381c5ad406f1c9a8193ee3cef1c8dba011b
darellanodev <darellano.aliaga@gmail.com>
2024-09-07 20:18:42 +0200

    #7 fix unit test

--- tests/unit/Day.test.ts
+++
@@ -1,7 +0,0 @@
-import { Day } from "../../src/Day"
-test("day is on", () => {
-  const day = new Day("5/2/2024")
-
-  const result = day.isOn
-  expect(result).toBeFalsy()
-})
---
+++ tests/unit/DayItem.test.ts
@@ -0,0 +1,8 @@
+import { DayItem } from '../../src/DayItem'
+test('day is completed', () => {
+  const completed = true
+  const day = new DayItem(1, '5/2/2024', completed)
+
+  const result = day.completed
+  expect(result).toBeTruthy()
+})
