118aa08f75a46b9c4fd228639bfe8077d652539b
darellanodev <darellano.aliaga@gmail.com>
2024-09-08 18:18:32 +0200

    closes #10 show the stats numbers passed to the Stat component

--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -16,15 +16,15 @@ export const Stats: React.FC<StatsProps> = ({
     <div className="stats-container">
       <div id="total-contributions">
         <div className="title">Total contributions</div>
-        <div id="total-contributions-number">12</div>
+        <div id="total-contributions-number">{totalContributions}</div>
       </div>
       <div id="current-streak">
         <div className="title">Current streak</div>
-        <div id="current-streak-number">12</div>
+        <div id="current-streak-number">{currentStreak}</div>
       </div>
       <div id="longest-streak">
         <div className="title">Longest streak</div>
-        <div id="longest-streak-number">12</div>
+        <div id="longest-streak-number">{longestStreak}</div>
       </div>
     </div>
   )
