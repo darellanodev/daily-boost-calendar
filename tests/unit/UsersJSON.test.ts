0bde34be521d5c2772acdeedb76bdbe51e1c12d5
darellanodev <darellano.aliaga@gmail.com>
2024-09-16 17:58:24 +0200

    #14 refactor tests

--- tests/unit/UsersJSON.test.ts
+++ tests/unit/UsersJSON.test.ts
@@ -1,14 +1,17 @@
 import { UsersJSON } from '../../src/models/UsersJSON'
 import { User } from '../../src/models/User'
 
+const getNewUser = () => {
+  const calendars = []
+  const userId = 152
+  return new User('john', 'myPass', userId, calendars)
+}
+
 test('create the json, when the users is "[]", returns the expected json', () => {
   const currentUsers = '[]'
 
-  const calendars = []
-  const userId = 152
-  const user = new User('john', 'myPass', userId, calendars)
   const usersJSON = new UsersJSON(currentUsers)
-
+  const user = getNewUser()
   const result = usersJSON.create(user.json)
 
   const expected = `[{"username": "john", "password": "myPass", "id": 152, "calendars": []}]`
@@ -19,11 +22,8 @@ test('create the json, when there is one user, returns the expected json', () =>
   const currentUsers =
     '[{"username": "mary", "password": "maryPass", "id": 28, "calendars": []}]'
 
-  const calendars = []
-  const userId = 152
-  const user = new User('john', 'myPass', userId, calendars)
   const usersJSON = new UsersJSON(currentUsers)
-
+  const user = getNewUser()
   const result = usersJSON.create(user.json)
 
   const expected = `[{"username": "mary", "password": "maryPass", "id": 28, "calendars": []},{"username": "john", "password": "myPass", "id": 152, "calendars": []}]`
@@ -32,30 +32,20 @@ test('create the json, when there is one user, returns the expected json', () =>
 
 test('exists, when the new user has a username that already exists in the user database, returns true', () => {
   const currentUsers =
-    '[{"username": "mary", "password": "maryPass", "id": 28, "calendars": []}]'
+    '[{"username": "john", "password": "maryPass", "id": 28, "calendars": []}]'
 
-  const calendars = []
-  const userId = 152
-  const newUsername = 'mary'
-  const user = new User(newUsername, 'myPass', userId, calendars)
   const usersJSON = new UsersJSON(currentUsers)
-
-  const result = usersJSON.exists(newUsername)
+  const result = usersJSON.exists('john')
 
   expect(result).toBeTruthy()
 })
 
 test('exists, when the new user has a username that does not exists yet in the database, returns true', () => {
   const currentUsers =
-    '[{"username": "mary", "password": "maryPass", "id": 28, "calendars": []}]'
+    '[{"username": "john", "password": "maryPass", "id": 28, "calendars": []}]'
 
-  const calendars = []
-  const userId = 152
-  const newUsername = 'john'
-  const user = new User(newUsername, 'myPass', userId, calendars)
   const usersJSON = new UsersJSON(currentUsers)
-
-  const result = usersJSON.exists(newUsername)
+  const result = usersJSON.exists('mary')
 
   expect(result).toBeFalsy()
 })
