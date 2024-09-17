bc9af57b250a49a93e2d75f048d09e0eae06b96d
darellanodev <darellano.aliaga@gmail.com>
2024-09-17 21:12:25 +0200

    #16 write the information in the FirstRun component and change some CSS styles

--- src/components/FirstRun/index.tsx
+++ src/components/FirstRun/index.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import { Header } from '../Header'
 import './firstRun.css'
+import { Button } from '../Button'
+
+interface FirstRunProps {
+  onContinue: () => void
+}
 
 type User = {
   name: string
 }
 
-export const FirstRun: React.FC = () => {
-  const [user, setUser] = React.useState<User>()
-  const [errorMessage, setErrorMessage] = useState<string>('')
+export const FirstRun: React.FC<FirstRunProps> = ({ onContinue }) => {
+  const user: User = { name: '' }
 
   return (
     <div id="login-content">
@@ -17,12 +21,23 @@ export const FirstRun: React.FC = () => {
         onLogin={() => {}}
         onLogout={() => {}}
         onCreateAccount={() => {}}
+        showButtons={false}
       />
 
       <article>
         <section>
           <div className="first-run-container">
-            <h1>Welcome to the app</h1>
+            <h1>Welcome to the first run of Daily Boost Calendar</h1>
+            <div>
+              <ul>
+                <li>✅ creating a demo user (testuser) to test the app</li>
+              </ul>
+            </div>
+            <p>
+              You can login with the testuser (username:{' '}
+              <strong>testuser</strong> / password: <strong>testpass</strong>)
+            </p>
+            <Button primary onClick={onContinue} label="Continue" />
           </div>
         </section>
       </article>
