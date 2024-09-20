3a04356e64df8b34e7dff92c5ef730e63482b930
darellanodev <darellano.aliaga@gmail.com>
2024-09-20 17:19:33 +0200

    #11 create an UserManager class

---
+++ src/models/UsersManager.ts
@@ -0,0 +1,69 @@
+import { CalendarItem } from './CalendarItem'
+import { User } from './User'
+
+export class UsersManager {
+  #initialUsers: string | null
+  constructor(initialUsers: string | null) {
+    this.#initialUsers = initialUsers
+  }
+  create(userJSON: string): string {
+    let users = this.#initialUsers
+    if (users === null || users === '') {
+      users = '[]'
+    }
+
+    let finalUsersJSON = ''
+
+    if (users === '[]') {
+      finalUsersJSON = '[' + userJSON + ']'
+    } else {
+      if (JSON.parse(users).length > 0) {
+        finalUsersJSON = users.slice(0, -2) + '},' + userJSON + ']'
+      } else {
+        finalUsersJSON = '[' + userJSON + ']'
+      }
+    }
+
+    return finalUsersJSON
+  }
+  exists(username: string): boolean {
+    if (this.#initialUsers === null || this.#initialUsers === '') {
+      return false
+    }
+
+    const users = JSON.parse(this.#initialUsers)
+
+    if (users.length === 0) {
+      return false
+    }
+
+    for (let user of users) {
+      if (user.username === username) {
+        return true
+      }
+    }
+    return false
+  }
+
+  getCalendars(username: string): CalendarItem[] {
+
+    if (this.#initialUsers === null || this.#initialUsers === '') {
+      return []
+    }
+
+    const users = JSON.parse(this.#initialUsers)
+
+    if (users.length === 0) {
+      return []
+    }
+
+    for (let user of users) {
+      if (user.username === username) {
+        return user.calendars
+      }
+    }
+
+    return []
+  
+  }
+}
---
+++ tests/unit/UsersManager.test.ts
@@ -0,0 +1,51 @@
+import { UsersManager } from '../../src/models/UsersManager'
+import { User } from '../../src/models/User'
+
+const getNewUser = () => {
+  const calendars = []
+  const userId = 152
+  return new User('john', 'myPass', userId, calendars)
+}
+
+test('create the json, when the users is "[]", returns the expected json', () => {
+  const currentUsers = '[]'
+
+  const usersManager = new UsersManager(currentUsers)
+  const user = getNewUser()
+  const result = usersManager.create(user.json)
+
+  const expected = `[{"username": "john", "password": "myPass", "id": 152, "calendars": []}]`
+  expect(result).toBe(expected)
+})
+
+test('create the json, when there is one user, returns the expected json', () => {
+  const currentUsers =
+    '[{"username": "mary", "password": "maryPass", "id": 28, "calendars": []}]'
+
+  const usersManager = new UsersManager(currentUsers)
+  const user = getNewUser()
+  const result = usersManager.create(user.json)
+
+  const expected = `[{"username": "mary", "password": "maryPass", "id": 28, "calendars": []},{"username": "john", "password": "myPass", "id": 152, "calendars": []}]`
+  expect(result).toBe(expected)
+})
+
+test('exists, when the new user has a username that already exists in the user database, returns true', () => {
+  const currentUsers =
+    '[{"username": "john", "password": "maryPass", "id": 28, "calendars": []}]'
+
+  const usersManager = new UsersManager(currentUsers)
+  const result = usersManager.exists('john')
+
+  expect(result).toBeTruthy()
+})
+
+test('exists, when the new user has a username that does not exists yet in the database, returns true', () => {
+  const currentUsers =
+    '[{"username": "john", "password": "maryPass", "id": 28, "calendars": []}]'
+
+  const usersManager = new UsersManager(currentUsers)
+  const result = usersManager.exists('mary')
+
+  expect(result).toBeFalsy()
+})
