2f456bad9e1673d5a83c7f476dc5630d4550129d
darellanodev <darellano.aliaga@gmail.com>
2024-10-06 19:40:53 +0200

    #11 Rename the property and create a getter method to return the days from an ActiveDays object.

--- src/models/ActiveDays.ts
+++ src/models/ActiveDays.ts
@@ -1,16 +1,20 @@
 export class ActiveDays {
-  #activeDaysList: string[]
-  constructor(activeDaysList: string[]) {
-    this.#activeDaysList = activeDaysList
+  #days: string[]
+  constructor(days: string[]) {
+    this.#days = days
   }
   get json(): string {
     let result = '['
-    for (const element of this.#activeDaysList) {
+    for (const element of this.#days) {
       result += `"${element}",`
     }
     result = result.slice(0, -1)
     result += ']'
 
     return result
+  }
+
+  get days(): string[] {
+    return this.#days
   }
 }
