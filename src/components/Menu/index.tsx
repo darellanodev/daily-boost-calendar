fc4b6ff846170c6471328a6020c02b04963beec7
darellanodev <darellano.aliaga@gmail.com>
2024-10-10 21:07:53 +0200

    #19 remove the unnecessary code from the Menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,9 @@
 import React, { useEffect } from 'react'
-import { Calendar } from '../Calendar'
 import { Header } from '../Header'
 import './menu.css'
-import { Stats } from '../Stats'
-import { DayItem } from '../../models/DayItem'
-import { DatesProvider } from '../../utils/DatesProvider'
 import { Tooltip } from 'react-tooltip'
 import { UsersManager } from '../../models/UsersManager'
 import { CalendarItem } from '../../models/CalendarItem'
-import { ActiveDays } from '../../models/ActiveDays'
 
 type User = {
   name: string
@@ -20,92 +15,11 @@ interface MenuProps {
 
 export const Menu: React.FC<MenuProps> = ({ onLogout }) => {
   const [user, setUser] = React.useState<User>()
-  const [calendarItem, setCalendarItem] = React.useState<CalendarItem | null>(
-    null,
-  )
-  const [daysItems, setDayItems] = React.useState<DayItem[]>([])
-  const [totalContributions, setTotalContributions] = React.useState<number>(0)
   const [calendars, setCalendars] = React.useState<CalendarItem[]>([])
-  const [idCalendar, setIdCalendar] = React.useState<number>(0)
-
-  let currentStreak = 0
-  let longestStreak = 0
-
-  const getDates = () => {
-    const totalWeeks = 23
-    const datesProvider = new DatesProvider(totalWeeks)
-    datesProvider.calculate()
-    return datesProvider.dates
-  }
-
-  const getDayItems = (dates: string[], activeDays: string[]) => {
-    const daysItems: DayItem[] = []
-    let i = 1
-    for (const date of dates) {
-      let completed = activeDays.includes(date)
-      daysItems.push(new DayItem(i, date, completed))
-      i++
-    }
-    return daysItems
-  }
-
-  const getTotalContributions = (daysItem: DayItem[]) => {
-    let totalContributions = 0
-    for (const dayItem of daysItem) {
-      if (dayItem.completed) {
-        totalContributions++
-      }
-    }
-    return totalContributions
-  }
 
   const handleLogout = (): void => {
     setUser(undefined)
     onLogout()
-  }
-
-  const loadCalendar = (idCalendar: number) => {
-    const currentCalendar: CalendarItem = calendars[idCalendar]
-    setCalendarItem(currentCalendar)
-
-    const dates = getDates()
-    const activeDays = getActiveDaysFromCalendar(idCalendar, calendars)
-    const daysItems = getDayItems(dates, activeDays.days)
-    setDayItems(daysItems)
-    setTotalContributions(getTotalContributions(daysItems))
-  }
-
-  const handleNextCalendar = () => {
-    setIdCalendar((prevIdCalendar) => {
-      const nextIdCalendar = prevIdCalendar + 1
-      if (calendars[nextIdCalendar] === undefined) {
-        return prevIdCalendar
-      }
-      loadCalendar(nextIdCalendar)
-
-      return nextIdCalendar
-    })
-  }
-
-  const handlePreviousCalendar = () => {
-    setIdCalendar((prevIdCalendar) => {
-      const nextIdCalendar = prevIdCalendar - 1
-      if (calendars[nextIdCalendar] === undefined) {
-        return prevIdCalendar
-      }
-      loadCalendar(nextIdCalendar)
-
-      return nextIdCalendar
-    })
-  }
-
-  const getActiveDaysFromCalendar = (id: number, calendars: CalendarItem[]) => {
-    if (Array.isArray(calendars[id].activeDays)) {
-      const aux: string[] = calendars[id].activeDays as string[]
-      return new ActiveDays(aux)
-    } else {
-      throw new Error('Error: activeDays is not an array')
-    }
   }
 
   useEffect(() => {
@@ -125,24 +39,11 @@ export const Menu: React.FC<MenuProps> = ({ onLogout }) => {
       throw new Error('Error: No calendars found')
     }
 
-    if (calendars[idCalendar] === null || calendars[idCalendar] === undefined) {
-      throw new Error('Error: calendars[idCalendar] is null')
-    }
-
-    const currentCalendar: CalendarItem = calendars[idCalendar]
-    setCalendarItem(currentCalendar)
-
     if (username) {
       setUser({ name: username })
     } else {
       setUser({ name: 'unknown' })
     }
-
-    const dates = getDates()
-    const activeDays = getActiveDaysFromCalendar(idCalendar, calendars)
-    const daysItems = getDayItems(dates, activeDays.days)
-    setDayItems(daysItems)
-    setTotalContributions(getTotalContributions(daysItems))
   }, [])
 
   return (
@@ -157,16 +58,9 @@ export const Menu: React.FC<MenuProps> = ({ onLogout }) => {
       <article>
         <section>
           <p className="help">Welcome to the Daily Boost Calendar.</p>
-          <button onClick={handlePreviousCalendar}>◀️ Previous Calendar</button>
-          <button onClick={handleNextCalendar}>Next Calendar ▶️</button>
-          <div className="taskslists">
-            <Calendar calendarItem={calendarItem} days={daysItems} />
-          </div>
-          <Stats
-            totalContributions={totalContributions}
-            currentStreak={currentStreak}
-            longestStreak={longestStreak}
-          />
+          <p className="help">
+            Click on the desired calendar from all the available to manage it.
+          </p>
         </section>
       </article>
     </div>
