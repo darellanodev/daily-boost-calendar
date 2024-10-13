8e518bc66db52f99f0936a0d68794f3ebac75b66
darellanodev <darellano.aliaga@gmail.com>
2024-10-13 20:10:26 +0200

    #19 create the basic elements for the miniCalendar

--- src/components/MiniCalendar/index.tsx
+++ src/components/MiniCalendar/index.tsx
@@ -10,8 +10,21 @@ interface MiniCalendarProps {
 
 export const MiniCalendar: React.FC<MiniCalendarProps> = ({ miniCalendar }) => {
   return (
-    <div className="minicalendar">
-      {miniCalendar.title} calendar
+    <div className="mini-calendar">
+      <header className="mini-calendar__header">
+        <div className="mini-calendar__logo">logo</div>
+        <h2 className="mini-calendar__title">{miniCalendar.title} calendar</h2>
+      </header>
+      <main className="mini-calendar__content">
+        <div className="mini-calendar__total-count">
+          <div className="mini-calendar__total-count-number">34</div>
+          <div className="mini-calendar__total-count-text">total</div>
+        </div>
+        <div className="mini-calendar__streak-count">
+          <div className="mini-calendar__total-count-number">78</div>
+          <div className="mini-calendar__total-count-text">day streak</div>
+        </div>
+      </main>
     </div>
   )
 }
