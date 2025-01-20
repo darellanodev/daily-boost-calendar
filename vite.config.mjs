694792bd7de44fbd7adbd9a8e9e2782ee1216006
darellanodev <darellano.aliaga@gmail.com>
2025-01-20 20:15:59 +0100

    Remove the vite-plugin-replace (#31)

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { replaceCodePlugin } from 'vite-plugin-replace'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    replaceCodePlugin({
-      replacements: [
-        {
-          from: 'src/assets',
-          to: 'assets',
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   base: './',
 })
